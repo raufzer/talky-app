c96dff593c0e71e15ec7a707caf4c268d1e976f8
raufzer <a_zerkhef@estin.dz>
2024-10-05 17:18:53 +0100

    chore: firebase initialize function integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter/material.dart';
 import 'package:talky_app/cubits/splash_screen_cubit/splash_screen_cubit.dart';
+import 'package:talky_app/firebase_options.dart';
 import 'package:talky_app/screens/splash_screen.dart';
 import 'package:talky_app/utils/screens_map.dart';
 
 
-void main() {
+Future<void> main() async {
   // WidgetsFlutterBinding.ensureInitialized();
   // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const TalkyApp());
 }
 
