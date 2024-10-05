c4e0f9d0741018ced5665ba98fa57012781e8666
raufzer <a_zerkhef@estin.dz>
2024-10-05 22:17:57 +0100

    feat(sign_up): set sign up service

---
+++ lib/services/sign_up_method.dart
@@ -0,0 +1,11 @@
+  import 'package:firebase_auth/firebase_auth.dart';
+
+Future<dynamic> signUpMethod(var email, var password) async {
+  
+    final credential = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(
+      email: email!,
+      password: password!,
+    );
+    return credential;
+  }
