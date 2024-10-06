d267c1581a14dd458f219227cca6f68033e8e006
raufzer <a_zerkhef@estin.dz>
2024-10-06 16:46:22 +0100

    refactor(ui): improve code & extract methods

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -3,6 +3,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:talky_app/services/sign_up_method.dart';
 import 'package:talky_app/utils/show_snack_bar.dart';
 import 'package:talky_app/widgets/custom_button.dart';
@@ -10,6 +11,8 @@ import 'package:talky_app/widgets/custom_title_widget.dart';
 import 'package:talky_app/widgets/input_field_widget.dart';
 import 'package:talky_app/widgets/sign_in_widget.dart';
 import 'package:talky_app/widgets/talky_logo_widget.dart';
+
+import '../utils/validators.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -22,70 +25,79 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   String? email;
   String? password;
+  bool isLoading = false;
   GlobalKey<FormState> formKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Form(
-        key: formKey,
-        child: ListView(
-          children: <Widget>[
-            const SizedBox(height: 80),
-            const TalkyLogo(
-              textColor: Color(0xFF243443),
-              dotColor: Color(0xFF377DFF),
-              textSize: 45,
-              dotSize: 15,
-              sizedBoxDot: 40,
-            ),
-            const SizedBox(height: 38),
-            const Center(
-              child: CustomTitleWidget(
-                titleText: 'Sign up',
-                titleSize: 16,
-                titleColor: Color(0xFF243443),
-                titleWeight: 'semi bold',
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Form(
+          key: formKey,
+          child: ListView(
+            children: <Widget>[
+              const SizedBox(height: 80),
+              const TalkyLogo(
+                textColor: Color(0xFF243443),
+                dotColor: Color(0xFF377DFF),
+                textSize: 45,
+                dotSize: 15,
+                sizedBoxDot: 40,
               ),
-            ),
-            const SizedBox(height: 38),
-            InputFieldWidget(
-              onChanged: (data) {
-                email = data;
-              },
-              hintText: 'Enter your gmail address',
-            ),
-            const SizedBox(height: 18),
-            InputFieldWidget(
-              onChanged: (data) {
-                password = data;
-              },
-              hintText: 'Enter your password',
-            ),
-            const SizedBox(height: 18),
-            const SizedBox(height: 160),
-            CustomButton(
-              text: 'Sign up',
-              onTap: () async {
-                if (formKey.currentState!.validate()) {
-                  try {
-                    var credential = await signUpMethod(email!, password!);
-                  } on FirebaseAuthException catch (e) {
-                    showSnackbar(
-                        e.message ?? 'There was an error signing up.', context);
-                  } catch (e) {
-                    showSnackbar('There was an error signing up.', context);
+              const SizedBox(height: 38),
+              const Center(
+                child: CustomTitleWidget(
+                  titleText: 'Sign up',
+                  titleSize: 16,
+                  titleColor: Color(0xFF243443),
+                  titleWeight: 'semi bold',
+                ),
+              ),
+              const SizedBox(height: 38),
+              InputFieldWidget(
+                hintText: 'Enter your email address',
+                validator: emailValidator,
+                onChanged: (data) {
+                  email = data;
+                },
+              ),
+              const SizedBox(height: 18),
+              InputFieldWidget(
+                hintText: 'Enter your password',
+                validator: passwordValidator,
+                onChanged: (data) {
+                  password = data;
+                },
+              ),
+              const SizedBox(height: 18),
+              const SizedBox(height: 160),
+              CustomButton(
+                text: 'Sign up',
+                onTap: () async {
+                  if (formKey.currentState!.validate()) {
+                    isLoading = true;
+                    try {
+                      var credential = await signUpMethod(email!, password!);
+                      // Handle successful sign-up here
+                    } on FirebaseAuthException catch (e) {
+                      showSnackbar(
+                          e.message ?? 'There was an error signing up.',
+                          context);
+                    } catch (e) {
+                      showSnackbar('There was an error signing up.', context);
+                    } finally {
+                      isLoading = false;
+                    }
                   }
-                } else {
-                  showSnackbar(
-                      'Please enter your email and password.', context);
-                }
-              },
-            ),
-            const SizedBox(height: 32),
-            const SignInWidget(),
-            const SizedBox(width: 100),
-          ],
+                },
+              ),
+              const SizedBox(height: 32),
+              const SignInWidget(),
+              const SizedBox(width: 100),
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/input_field_widget.dart
+++ lib/widgets/input_field_widget.dart
@@ -3,21 +3,18 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:talky_app/utils/constants.dart';
 
 class InputFieldWidget extends StatelessWidget {
-   InputFieldWidget(
-      {super.key, required this.hintText, this.onChanged});
-  Function(String)? onChanged;
+  const InputFieldWidget({super.key, required this.hintText, this.onChanged, this.validator});
+
   final String hintText;
+  final Function(String)? onChanged;
+  final String? Function(String?)? validator;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: TextFormField(
-        validator: (data){
-          if (data!.isEmpty) return 'Email address is required';
-          if (!data.contains('@')) return 'Invalid email address';
-          return null;
-        },
+        validator: validator,
         onChanged: onChanged,
         keyboardType: TextInputType.emailAddress,
         decoration: InputDecoration(
@@ -27,17 +24,17 @@ class InputFieldWidget extends StatelessWidget {
             color: const Color(0xFFAAB0B7),
           ),
           border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-              borderSide: const BorderSide(
-                color: Color(0xFFAAB0B7),
-                width: 1,
-              )),
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(
+              color: Color(0xFFAAB0B7),
+              width: 1,
+            ),
+          ),
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(8),
             borderSide: const BorderSide(color: blueColor),
           ),
-          contentPadding:
-              const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+          contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
         ),
       ),
     );
