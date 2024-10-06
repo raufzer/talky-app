75c4b9ee6ab81021f7e199a1ae752fb14425ebf8
raufzer <a_zerkhef@estin.dz>
2024-10-06 16:44:47 +0100

    refactor(email_password_validators): add email & passwords validators functions

---
+++ lib/utils/validators.dart
@@ -0,0 +1,23 @@
+String? emailValidator(String? value) {
+  if (value!.isEmpty) {
+    return 'Email address is required';
+  }
+
+  if (!RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$").hasMatch(value)) {
+    return 'Please enter a valid email address';
+  }
+
+  return null;
+}
+
+String? passwordValidator(String? value) {
+  if (value!.isEmpty) {
+    return 'Password is required';
+  }
+
+  if (value.length < 6) {
+    return 'Password must be at least 6 characters long';
+  }
+
+  return null;
+}
